24559079fdc30e20732fba0e8ee2171d96f740d0
yasakei <yas.studios.en@gmail.com>
2025-09-11 23:05:35 +0600

    improved pdf rendering and improved perfomance

--- !/lib/node_modules/@google/gemini-cli/dist/src/validateNonInterActiveAuth.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright 2025 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-import type { Config } from '@google/gemini-cli-core';
-import { AuthType } from '@google/gemini-cli-core';
-import { type LoadedSettings } from './config/settings.js';
-export declare function validateNonInteractiveAuth(configuredAuthType: AuthType | undefined, useExternalAuth: boolean | undefined, nonInteractiveConfig: Config, settings: LoadedSettings): Promise<Config>;
